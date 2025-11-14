6fdfcbe5aaee7047e93e80ec1d324994868465a0
Rainxch Zed <undefineduser087@gmail.com>
2025-11-14 19:25:11 +0500

    Add support for other platforms (ios, web, desktop)

--- composeApp/src/commonMain/kotlin/zed/rainxch/novemberminichallenges/App.kt
+++ composeApp/src/commonMain/kotlin/zed/rainxch/novemberminichallenges/App.kt
@@ -3,9 +3,12 @@ package zed.rainxch.novemberminichallenges
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import zed.rainxch.novemberminichallenges.long_press_compare.LongPressCompareRoot
+import zed.rainxch.novemberminichallenges.long_press_compare.LongPressCompareViewModel
 
 @Composable
 @Preview
 fun App() {
-    LongPressCompareRoot()
+    LongPressCompareRoot(
+        viewModel = LongPressCompareViewModel()
+    )
 }
