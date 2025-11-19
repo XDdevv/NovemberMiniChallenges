28f18866e10d91499cc031b149793bf62d9ee571
Rainxch Zed <undefineduser087@gmail.com>
2025-11-19 20:46:38 +0500

    Initialized Global Deals mini challenge

--- composeApp/src/commonMain/kotlin/zed/rainxch/novemberminichallenges/App.kt
+++ composeApp/src/commonMain/kotlin/zed/rainxch/novemberminichallenges/App.kt
@@ -4,11 +4,10 @@ import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import zed.rainxch.novemberminichallenges.circular_stock_tracker.presentation.CircularStockTrackerRoot
 import zed.rainxch.novemberminichallenges.circular_stock_tracker.presentation.CircularStockTrackerViewModel
+import zed.rainxch.novemberminichallenges.global_deals.presentation.GlobalDealsRoot
 
 @Composable
 @Preview
 fun App() {
-    CircularStockTrackerRoot(
-        viewModel = CircularStockTrackerViewModel()
-    )
+    GlobalDealsRoot()
 }
